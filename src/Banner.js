11c8328da7864c113409c88620f88814d469e9f9
Mohammad Parvez Musharaf <mdparvezmusharaf2@gmail.com>
2022-09-30 12:11:07 +0600

    day 1 almost done by home page interface

--- src/Banner.js
+++ src/Banner.js
@@ -1,10 +1,55 @@
-import React from 'react'
+import axios from "./axios";
+import React from "react";
+import { useState, useEffect } from "react";
 import "./Banner.css";
+import requests from "./Request";
+const base_url = "https://image.tmdb.org/t/p/original/";
+
 
 const Banner = () => {
-  return (
-    <div className='banner'>Banner</div>
-  )
-}
+  const [movie, setMovie] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length -1)
+        ]
+      );
+      return request;
+    }
+    fetchData();
+  }, []);
 
-export default Banner
+  console.log(movie);
+  function trancate(string, n) {
+    return string?.length > n ? string.substr(0, n - 1) + "..." : string;
+  }
+  return (
+    <div
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url('${base_url}${movie?.backdrop_path}')`,
+        backgroundPosition: "center",
+      }}
+    >
+      <div className="banner__contents">
+        <h1 className="banner__title">{movie?.name || movie?.title || movie?.original_name}</h1>
+        <div className="banner__buttons">
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
+        </div>
+        <h1 className="banner__description">
+          {trancate(
+            movie?.overview,
+            150
+          )}
+        </h1>
+      </div>
+      <div className="banner--fadeBottom" />
+    </div>
+  );
+};
+
+export default Banner;
--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,16 +1,29 @@
 import React from 'react'
 import "./HomeScreen.css"
-import Nav from './Nav'
-
+import Nav from './Nav';
+import Banner from './Banner';
+import requests from './Request';
+import Row from './Row';
 const HomeScreen = () => {
   return (
-    <div className='HomeScreen'>
-        <Nav />
-       
-        {/* Banner */}
-        {/* Row */}
+    <div className="HomeScreen">
+      <Nav />
+      <Banner />
+
+      <Row
+        isLargeRow
+        title="Netflix Originals"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Trending now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTrending} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
-  )
+  );
 }
 
 export default HomeScreen
